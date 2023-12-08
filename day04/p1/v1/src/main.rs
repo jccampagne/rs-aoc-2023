690b91aa2055d2b5fc6a5ca95cdf0e280051d05d
Jean-Charles Campagne <jc.campagne@icloud.com>
2023-12-08 16:31:26 +0000

    wip day 04 p1

--- day04/p1/v1/src/main.rs
+++ day04/p1/v1/src/main.rs
@@ -1,12 +1,18 @@
-use std::collections::HashSet;
+use std::{collections::HashSet, env, fs};
 
 fn main() {
-    println!("Hello, world!");
+    let mut args = env::args();
+    args.next();
+    let input_file = args.next().unwrap();
+    let contents = fs::read_to_string(input_file).unwrap();
+    let result = parse(&contents);
+    dbg!(result);
 }
 
 type Number = i32;
 
 fn parse_line(line: &str) -> Number {
+    // static RE: Lazy<Regex> = Lazy::new(|| Regex::new(r"\d+").unwrap());
     println!("========= line = {line}");
     let mut parts = line.split(":");
     let _game_id = parts.next();
@@ -14,10 +20,15 @@ fn parse_line(line: &str) -> Number {
     let mut numbers = num_part.split(" ");
     let mut winning = HashSet::new();
     while let Some(n) = numbers.next() {
-        if n == "|" { break;}
+        if n == "" {
+            continue;
+        }
+        if n == "|" {
+            break;
+        }
         winning.insert(n);
     }
-    let mut score : i32 = 0;
+    let mut score: i32 = 0;
     while let Some(n) = numbers.next() {
         if winning.contains(n) {
             if score == 0 {
@@ -53,6 +64,18 @@ Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
 
         let result = parse(input);
         let expected = 13;
+
+        assert_eq!(result, expected)
+    }
+
+    #[test]
+    fn test_example_2() {
+        let input = "\
+        Card  22: 78 39 74 11 41 24  9 33 68 45 | 41 66 29 79 15 90 62 60 95 69 57 55 81 33 20 89 76 65 56 77 61 18 88 75 19
+        Card  23: 64 16 33 77 80 89 90 50 57  4 | 46 94 45 42 82 61 67  3 76 74 96 79  7 70 73 97 11 34 35 72 55 51 36 12 13
+";
+        let result = parse(input);
+        let expected = 2;
 
         assert_eq!(result, expected)
     }
