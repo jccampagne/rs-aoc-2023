d371df975317901a6a64f0352d39ac88cd971cfa
Jean-Charles Campagne <jc.campagne@icloud.com>
2023-12-25 09:58:46 +0100

    wip

---
+++ day09/p1/v1/src/main.rs
@@ -0,0 +1,66 @@
+use core::num;
+
+fn main() {
+    println!("Hello, world!");
+}
+
+type Number = i32;
+type VNumbers = Vec<Number>;
+
+fn parse(input: &str) -> Number {
+    let lines = input.lines();
+    lines.map(parse_line).sum()
+}
+
+fn parse_line(line: &str) -> Number {
+    let numbers: Result<Vec<Number>, _> = line
+        .split_whitespace()
+        .map(|c| c.parse::<Number>())
+        .collect();
+    let numbers = numbers.unwrap();
+    let prediction = predict(&numbers);
+    prediction
+}
+
+fn predict(numbers: &Vec<Number>) -> Number {
+    dbg!(numbers);
+    let mut all_diffs: Vec<VNumbers> = Vec::new();
+    let mut do_loop = true;
+    // let mut current: &VNumbers = numbers;
+    while do_loop {
+    let mut current: &VNumbers = numbers;
+    do_loop = false;
+        let mut iter = current.iter();
+        let a = iter.next().unwrap();
+        let mut diffs: VNumbers = Vec::new();
+        // current = &diffs;
+        while let Some(b) = iter.next() {
+            let d = b - a;
+            diffs.push(d);
+            if d != 0 {
+                do_loop = true
+            }
+        }
+
+        all_diffs.push(diffs);
+        // current = &all_diffs.last().unwrap()
+    }
+    dbg!(all_diffs);
+    todo!()
+}
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_parse() {
+        let input = "\
+0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45";
+
+        let result = parse(input);
+        let expected = 114;
+        assert_eq!(result, expected);
+    }
+}
